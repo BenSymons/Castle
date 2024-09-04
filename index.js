f7445d235b25d17836bfa1422bcaec34d0c92066
vaughandroid <chris.vaughan01@bbc.co.uk>
2024-09-04 15:18:25 +0100

    <Insert commit message here>

--- index.js
+++ index.js
@@ -1,7 +1,54 @@
 "use strict";
+var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
+    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
+        if (ar || !(i in from)) {
+            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
+            ar[i] = from[i];
+        }
+    }
+    return to.concat(ar || Array.prototype.slice.call(from));
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = castle;
+var EMPTY_SQUARE = '_';
+var CASTLE = 'C';
 function castle(board) {
-    return;
+    var _a = findCastlePosition(board), x = _a[0], y = _a[1];
+    var pieces = __spreadArray(__spreadArray([], getClosestPiecesToCastle(getPiecesOnColumn(x, board)), true), getClosestPiecesToCastle(getPiecesOnRow(y, board)), true);
+    return pieces.length > 0 ? pieces : false;
 }
 ;
+function findCastlePosition(board) {
+    for (var y = 0; y < board.length; y++) {
+        for (var x = 0; x < board[y].length; x++) {
+            if (board[y][x] === CASTLE) {
+                return [x, y];
+            }
+        }
+    }
+    throw new Error('Failed to find the castle!');
+}
+function getPiecesOnColumn(x, board) {
+    var pieces = [];
+    for (var y = 0; y < board.length; y++) {
+        var piece = board[y][x];
+        if (piece !== EMPTY_SQUARE) {
+            pieces.push(piece);
+        }
+    }
+    return pieces;
+}
+function getPiecesOnRow(y, board) {
+    var pieces = [];
+    for (var x = 0; x < board[y].length; x++) {
+        var piece = board[y][x];
+        if (piece !== EMPTY_SQUARE) {
+            pieces.push(piece);
+        }
+    }
+    return pieces;
+}
+function getClosestPiecesToCastle(pieces) {
+    var castlePosition = pieces.indexOf(CASTLE);
+    return pieces.slice(Math.max(castlePosition - 1, 0), Math.min(castlePosition + 2, pieces.length)).filter(function (piece) { return piece !== CASTLE; });
+}
--- index.ts
+++ index.ts
@@ -1,3 +1,55 @@
-export default function castle(board: any) {
-  return;
+type Board = string[][];
+
+const EMPTY_SQUARE = '_';
+const CASTLE = 'C';
+
+export default function castle(board: Board): string[] | false {
+  const [x, y] = findCastlePosition(board);
+  const pieces = [
+    ...getClosestPiecesToCastle(getPiecesOnColumn(x, board)),
+    ...getClosestPiecesToCastle(getPiecesOnRow(y, board))
+  ];
+
+  return pieces.length > 0 ? pieces : false;
 };
+
+function findCastlePosition(board: Board): number[] {
+  for (let y = 0; y < board.length; y++) {
+    for (let x = 0; x < board[y].length; x++) {
+      if (board[y][x] === CASTLE) {
+        return [x, y];
+      }
+    }
+  }
+  throw new Error('Failed to find the castle!');
+}
+
+function getPiecesOnColumn(x: number, board: Board): string[] {
+  const pieces = [];
+  for (let y = 0; y < board.length; y++) {
+    let piece = board[y][x];
+    if (piece !== EMPTY_SQUARE) {
+      pieces.push(piece);
+    }
+  }
+  return pieces;
+}
+
+function getPiecesOnRow(y: number, board: Board): string[] {
+  const pieces = [];
+  for (let x = 0; x < board[y].length; x++) {
+    let piece = board[y][x];
+    if (piece !== EMPTY_SQUARE) {
+      pieces.push(piece);
+    }
+  }
+  return pieces;
+}
+
+function getClosestPiecesToCastle(pieces: string[]): string[] {
+  const castlePosition = pieces.indexOf(CASTLE);
+  return pieces.slice(
+    Math.max(castlePosition - 1, 0),
+    Math.min(castlePosition + 2, pieces.length)
+  ).filter((piece) => piece !== CASTLE);
+}
