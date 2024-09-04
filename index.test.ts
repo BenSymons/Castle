f95d29774df0de087152334d4eb8d52ee934020f
BenSymons <ben.symons@bbc.co.uk>
2024-09-04 08:58:42 +0100

    feat: build kata

---
+++ index.test.ts
@@ -0,0 +1,36 @@
+import castle from ".";
+import boards from './boards.json'
+
+describe('basic tests', () => {
+  it('returns false if no pieces can be captured', () => {
+    expect(castle(boards[0])).toBe(false);
+    expect(castle(boards[1])).toBe(false);
+  });
+  it(`Given a piece is piece is available horizontally
+      castle will return that piece in an array`, () => {
+    expect(castle(boards[2])).toEqual(['B']);
+    expect(castle(boards[3])).toEqual(['K']);
+  });
+  it(`Given a piece is available vertically
+      castle will return that piece in an array`, () => {
+    expect(castle(boards[4])).toEqual(['Q']);
+    expect(castle(boards[5])).toEqual(['P']);
+  });
+});
+
+describe('advanced tests', () => {
+  it(`Given pieces are available both horizontally and vertically
+    castely will return those pieces in an array`, () => {
+    const expected = castle(boards[6]);
+    expect(expected).toContain(['K']);
+    expect(expected).toContain(['P']);
+  });
+  it(`Given pieces are available both horizontally and vertically
+      And there are other pieces on the row/column that are not available
+      Then castle returns an array of only the available pieces`, () => {
+    const expected = castle(boards[7]);
+    expect(expected).toContain(['K']);
+    expect(expected).toContain(['P']);
+    expect(expected).toContain(['Q']);
+  })
+})
